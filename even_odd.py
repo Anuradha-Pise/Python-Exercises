bee0dc986270e333b70369365f0f315f39f9f737
Anuradha-Pise <70841033+Anuradha-Pise@users.noreply.github.com>
2022-08-18 22:59:49 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,7 @@
+# Python program to check whether the given number is even or not.
+
+number = int(input("Enter a number:"))
+if number%2 ==0:
+    print("The number is Even.")
+else:
+    print("The number is Odd.")
