c783370a83794a9592896e1b5d67f0f38e8b0d19
Anuradha-Pise <70841033+Anuradha-Pise@users.noreply.github.com>
2022-08-19 06:29:34 +0530

    Add files via upload

---
+++ degree_to_fahrenheit.py
@@ -0,0 +1,4 @@
+# Python program to convert the temperature in degree centigrade to Fahrenheit
+c = int(input("Enter temperature in Centigrade:"))
+f = (9*c/5)+32
+print("Temperature in Fahrenheit is: ",f)
